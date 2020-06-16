4ab161927438ca578cd7a60a9601c2b82fc17c94
Benjamin Jackson <bhjackson@gmail.com>
2020-06-16 11:41:45 -0400

    Added additional filler phrases to the list of stripped expressions.

--- clean.sh
+++ clean.sh
@@ -4,5 +4,6 @@ cp "$INPUT_FILE" "$INPUT_FILE.original"
 
 perl -i -pe 's/ kind of like / /g' "$INPUT_FILE"
 perl -i -pe 's/(?<!hat) kind of / /g' "$INPUT_FILE"
-perl -i -pe 's/(?:Um|Like),\s*([a-zA-Z])/\U$1/g' "$INPUT_FILE"
-perl -i -pe 's/(?:um|like),\s*([a-zA-Z])/$1/g' "$INPUT_FILE"
+perl -i -pe 's/(?:Um|Like|You know),\s*([a-zA-Z])/\U$1/g' "$INPUT_FILE"
+perl -i -pe 's/,?(\s)?(?:um|like|you know|I think),\s*([a-zA-Z])/$1$2/g' "$INPUT_FILE"
+perl -i -pe 's/Gotcha\.\s//g' "$INPUT_FILE"
